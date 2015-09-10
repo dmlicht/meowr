9673db51f93da6650acb08508d40c20b0309ae0b
Jackson <kiro705@Jackson-Suis-MacBook-Pro.local>
2015-09-10 12:55:44 -0400

    Pulled cat picture request into function

--- www/views/people/people.js
+++ www/views/people/people.js
@@ -2,24 +2,53 @@ angular.module('starter')
 .controller('CardsCtrl', function ($scope, $http, $ionicLoading, $ionicSideMenuDelegate, TDCardDelegate) {
   console.log('CARDS CTRL');
   $ionicSideMenuDelegate.canDragContent(false);
-  var cats = [];
-  $ionicLoading.show();
-  $http.get('https://randomuser.me/api/?results=5').success(function (response) {
-    console.log(JSON.stringify(response.results));
+  $scope.cards = [];
+  //var cats = [];
+  //var moreCats = [];
+  //$ionicLoading.show();
 
-      angular.forEach(response.results, function (result) {
-        cats.push(result);
-        //console.log(JSON.stringify(result));
-      });
+  //$http.get('https://randomuser.me/api/?results=5').success(function (response) {
+  //    angular.forEach(response.results, function (result) {
+  //      cats.push(result);
+  //    });
+  //    $ionicLoading.hide();
+  //  }).error(function (err) {
+  //    console.log(err);
+  //  });
+
+  addCats = function(){
+    $ionicLoading.show();
+    $http.get('https://randomuser.me/api/?results=5').success(function (response) {
+      $scope.cards.push.apply($scope.cards, response.results);
+      //angular.forEach(response.results, function (result) {
+      //  cats.push(result);
+      //});
       $ionicLoading.hide();
     }).error(function (err) {
       console.log(err);
     });
+  };
+
+  addCats();
 
   //$scope.cards = Array.prototype.slice.call(cats, 0);
-  $scope.cards = cats;
+  //$scope.cards = cats;
   $scope.cardDestroyed = function(index) {
     $scope.cards.splice(index, 1);
+    //if (cats.length<2){
+    //  $http.get('https://randomuser.me/api/?results=5').success(function (response) {
+    //    console.log(JSON.stringify(response.results));
+    //
+    //    angular.forEach(response.results, function (result) {
+    //      moreCats.push(result);
+    //      //console.log(JSON.stringify(result));
+    //    });
+    //    cats = cats.concat(moreCats);
+    //    $scope.cards = cats;
+    //    $ionicLoading.hide();
+    //  }).error(function (err) {
+    //    console.log(err);
+    //  });
   };
 
   $scope.addCard = function() {
