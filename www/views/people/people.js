1fff5bf2681b3080023f60088b04f3771b564a13
Jackson <kiro705@Jackson-Suis-MacBook-Pro.local>
2015-09-10 13:21:53 -0400

    allowed cats to refreash without loading symbol

--- www/views/people/people.js
+++ www/views/people/people.js
@@ -3,26 +3,13 @@ angular.module('starter')
   console.log('CARDS CTRL');
   $ionicSideMenuDelegate.canDragContent(false);
   $scope.cards = [];
-  //var cats = [];
-  //var moreCats = [];
-  //$ionicLoading.show();
-
-  //$http.get('https://randomuser.me/api/?results=5').success(function (response) {
-  //    angular.forEach(response.results, function (result) {
-  //      cats.push(result);
-  //    });
-  //    $ionicLoading.hide();
-  //  }).error(function (err) {
-  //    console.log(err);
-  //  });
 
   addCats = function(){
-    $ionicLoading.show();
+    if ($scope.cards.length < 1){
+      $ionicLoading.show();
+    }
     $http.get('https://randomuser.me/api/?results=5').success(function (response) {
-      $scope.cards.push.apply($scope.cards, response.results);
-      //angular.forEach(response.results, function (result) {
-      //  cats.push(result);
-      //});
+      $scope.cards.unshift.apply($scope.cards, response.results);
       $ionicLoading.hide();
     }).error(function (err) {
       console.log(err);
@@ -31,40 +18,19 @@ angular.module('starter')
 
   addCats();
 
-  //$scope.cards = Array.prototype.slice.call(cats, 0);
-  //$scope.cards = cats;
   $scope.cardDestroyed = function(index) {
     $scope.cards.splice(index, 1);
-    //if (cats.length<2){
-    //  $http.get('https://randomuser.me/api/?results=5').success(function (response) {
-    //    console.log(JSON.stringify(response.results));
-    //
-    //    angular.forEach(response.results, function (result) {
-    //      moreCats.push(result);
-    //      //console.log(JSON.stringify(result));
-    //    });
-    //    cats = cats.concat(moreCats);
-    //    $scope.cards = cats;
-    //    $ionicLoading.hide();
-    //  }).error(function (err) {
-    //    console.log(err);
-    //  });
+    if ($scope.cards.length < 2) {
+      addCats();
+    }
   };
-
-  $scope.addCard = function() {
-    var newCard = cats[Math.floor(Math.random() * cats.length)];
-    newCard.id = Math.random();
-    $scope.cards.push(angular.extend({}, newCard));
-  }
 
   $scope.yesCard = function() {
     console.log('YES');
-    $scope.addCard();
   };
 
   $scope.noCard = function() {
     console.log('NO');
-    $scope.addCard();
   };
   $scope.toggleLeft = function() {
   $ionicSideMenuDelegate.toggleLeft();
@@ -73,10 +39,8 @@ angular.module('starter')
 .controller('CardCtrl', function($scope, TDCardDelegate) {
   $scope.cardSwipedLeft = function(index) {
     console.log('LEFT SWIPE');
-    $scope.addCard();
   };
   $scope.cardSwipedRight = function(index) {
     console.log('RIGHT SWIPE');
-    $scope.addCard();
   };
 })
